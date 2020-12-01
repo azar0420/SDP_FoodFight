ca7043734f33e908fd18157acd6359bcd6affee6
azar0420 <70549457+azar0420@users.noreply.github.com>
2020-12-01 13:45:12 -0500

    Update main.cpp

--- SDP_FoodFight/main.cpp
+++ SDP_FoodFight/main.cpp
@@ -1,15 +1,36 @@
 #include "FEHLCD.h"
 
-/**
- * Entry point to the application.
- * 
- * @returns status code of program exit
- */
+#define X_MAX 319
+#define Y_MAX 239
+
+void MenuStartUp();
+
 int main() {
+    int x, y;
     LCD.Clear();
-    LCD.WriteLine("Hello World!");
-    while (1) {
+    //LCD.WriteLine("Hello World!");
+    MenuStartUp();
+    while (!LCD.Touch(&x, &y)) {
         // Run forever
     }
     return 0;
+}
+
+void MenuStartUp() {
+    
+    //play game option
+    LCD.DrawRectangle(100,20, 150, 40);
+    LCD.WriteAt("Play Game", 110, 25);
+
+    //view stats option
+    LCD.DrawRectangle(100,70,150,40);
+    LCD.WriteAt("View Statistics", 110, 75);
+
+    //view instructions option
+    LCD.DrawRectangle(100,120,150,40);
+    LCD.WriteAt("View Instructions", 110, 125);
+
+    //view credits option
+    LCD.DrawRectangle(100,170, 150,40);
+    LCD.WriteAt("View Credits", 110, 175);
 }
